24e14f554bb098cf20cd1e6832a68127d35fac4e
Saravanabalaji <dsbalaji2001@gmail.com>
2025-02-24 13:50:29 +0530

    routes added for admin and teacher module new api

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { createAdmin, adminLogin, createClass, allocateClass } = require("../controllers/adminController");
+const { createAdmin, adminLogin, createClass, allocateClass, getclass } = require("../controllers/adminController");
 const router = express.Router();
 
 router.post("/create", createAdmin);
 router.post("/login", adminLogin);
 router.post('/createClass', createClass);
+router.get('/getClass', getclass);
 router.post('/allocateClass', allocateClass);
 
 module.exports = router;
--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express.Router();
-const { createTeacher, teacherLogin } = require('../controllers/teacherController');
+const { createTeacher, teacherLogin, getTeachers } = require('../controllers/teacherController');
 
 app.post('/createTeacher', createTeacher);
+app.get('/getTeacher', getTeachers);
 app.post('/loginTeacher', teacherLogin);
 
 module.exports = app;
